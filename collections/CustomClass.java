7e7ce0fa3eaa35d7c58b1be35e54989b69d50a88
jyotishankar04 <patrajyotishankar@gmail.com>
2026-01-30 22:44:26 +0530

    java request handler

---
+++ collections/CustomClass.java
@@ -0,0 +1,18 @@
+class Data{
+    public  Integer num;
+    public String name;
+
+    public Data(int _num, String _name) {
+        this.num  = 
+    }
+}
+
+
+
+public class CustomClass {
+    public static void main(String[] args) {
+        Integer num = 5;
+        String name = "Raj";
+
+    }
+}
